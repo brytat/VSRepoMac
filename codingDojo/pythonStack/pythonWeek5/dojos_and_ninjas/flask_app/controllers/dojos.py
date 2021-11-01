711a7573fab000c9c298922e0477e03a2376098e
brytat <brytontateishi@gmail.com>
2021-10-31 23:13:10 -0700

    Updated Dojos and Ninjas Assignment

--- codingDojo/pythonStack/pythonWeek5/dojos_and_ninjas/flask_app/controllers/dojos.py
+++ codingDojo/pythonStack/pythonWeek5/dojos_and_ninjas/flask_app/controllers/dojos.py
@@ -1,8 +1,5 @@
-from flask import redirect, render_template
-from werkzeug.exceptions import RequestURITooLarge
-
+from flask import redirect, render_template, request
 from flask_app import app
-
 from flask_app.models.dojo import Dojo
 
 @app.route('/')
@@ -11,19 +8,19 @@ def index():
 
 @app.route("/dojos")
 def show():
-    dojos = Dojo.get_dojo_with_ninjas()
+    dojos = Dojo.get_all()
     print(dojos)
     return render_template("dojos.html", all_dojos = dojos)
-            
-@app.route("/ninjas")
-def new():
-    dojos = Dojo.get_all()
-    return render_template("new_ninja.html", all_dojos = dojos)
 
-# @app.route('/dojos/<int:id>')
-# def render_edit(id):
-#     data ={
-#         "id":id
-#     }
-#     print(id)
-#     return render_template("dojo_show.html", Dojo = Dojo.get_one(data))
+@app.route("/creatr/dojo", methods=['POST'])
+def create_dojo():
+    Dojo.save(request.form)
+    return redirect('/dojos')
+
+@app.route('/dojos/<int:id>')
+def show_dojo(id):
+    data ={
+        "id":id
+    }
+    print(id)
+    return render_template("dojo_show.html", dojo = Dojo.get_dojo_with_ninjas(data))
--- codingDojo/pythonStack/pythonWeek5/dojos_and_ninjas/flask_app/models/dojo.py
+++ codingDojo/pythonStack/pythonWeek5/dojos_and_ninjas/flask_app/models/dojo.py
@@ -10,9 +10,19 @@ class Dojo:
         self.ninjas = []
 
     @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM dojos;"
+        results = connectToMySQL('dojos_and_ninjas_schema').query_db(query)
+        dojos = []
+        for d in results:
+            dojos.append( cls(d) )
+        return dojos
+
+    @classmethod
     def save(cls, data):
         query = "INSERT INTO dojos (name, created_at, Updated_at) VALUES ( %(name)s, NOW(), NOW() );"
-        return connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
+        result = connectToMySQL('dojos_and_ninjas_schema').query_db(query, data)
+        return result
 
     @classmethod
     def get_dojo_with_ninjas(cls, data):
@@ -30,19 +40,3 @@ class Dojo:
             }
             dojo.ninjas.append( ninja.Ninja( ninja_data ) )
         return dojo
-
-    # @classmethod
-    # def get_one(cls, data ):
-    #     query  = "SELECT * FROM dojos WHERE id = %(id)s";
-    #     result = connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
-    #     return cls(result[0])
-
-    # @classmethod
-    # def update(cls,data):
-    #     query = "UPDATE dojos SET name=%(name)s, updated_at=NOW() WHERE id = %(id)s;"
-    #     return connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
-
-    # @classmethod
-    # def destroy(cls,data):
-    #     query  = "DELETE FROM dojos WHERE id = %(id)s;"
-    #     return connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
