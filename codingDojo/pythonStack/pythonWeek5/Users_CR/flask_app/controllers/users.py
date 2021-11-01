eb4a2afdc66382843ad1d85a733022b64b2ebe6d
brytat <brytontateishi@gmail.com>
2021-10-31 20:19:05 -0700

    CRUD Assignment updated

---
+++ codingDojo/pythonStack/pythonWeek5/Users_CR/flask_app/controllers/users.py
@@ -0,0 +1,52 @@
+from flask import redirect, render_template, request
+
+from flask_app import app
+
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route("/users")
+def users():
+    users = User.get_all()
+    print(users)
+    return render_template("read(all).html", all_users = users)
+            
+@app.route("/user/new")
+def new():
+    return render_template("create.html")
+
+@app.route("/user/process", methods=['POST'])
+def process_form():
+    User.save(request.form)
+    return redirect('/users')
+
+@app.route('/user/edit/<int:id>')
+def render_edit(id):
+    data ={
+        "id":id
+    }
+    print(id)
+    return render_template("edituser.html", one_user = User.get_one(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("read(one).html", user = User.get_one(data))
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id':id
+    }
+    User.destroy(data)
+    return redirect('/users')
--- codingDojo/pythonStack/pythonWeek5/Users_CR/server.py
+++ codingDojo/pythonStack/pythonWeek5/Users_CR/server.py
@@ -1,55 +1,5 @@
-from flask import Flask, redirect, render_template, request
-# import the class from users.py
-from user import User
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return redirect('/users')
-
-@app.route("/users")
-def users():
-    users = User.get_all()
-    print(users)
-    return render_template("read(all).html", all_users = users)
-            
-@app.route("/user/new")
-def new():
-    return render_template("create.html")
-
-@app.route("/user/process", methods=['POST'])
-def process_form():
-    User.save(request.form)
-    return redirect('/users')
-
-@app.route('/user/edit/<int:id>')
-def render_edit(id):
-    data ={
-        "id":id
-    }
-    print(id)
-    return render_template("edituser.html", one_user = User.get_one(data))
-
-@app.route('/user/show/<int:id>')
-def show(id):
-    data ={ 
-        "id":id
-    }
-    return render_template("read(one).html", user = User.get_one(data))
-
-@app.route('/user/update',methods=['POST'])
-def update():
-    User.update(request.form)
-    return redirect('/users')
-
-@app.route('/user/destroy/<int:id>')
-def destroy(id):
-    data ={
-        'id':id
-    }
-    User.destroy(data)
-    return redirect('/users')
+from flask_app.controllers import users
+from flask_app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
