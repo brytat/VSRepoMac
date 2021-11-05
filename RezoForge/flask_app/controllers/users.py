96a4d7d5fb168551cbfb0bf72672f6de4f8f3b3f
brytat <brytontateishi@gmail.com>
2021-11-05 00:02:39 -0700

    RezoForge Update: Adding Validation

--- RezoForge/flask_app/controllers/users.py
+++ RezoForge/flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 
 from flask_app import app
 
@@ -6,10 +6,20 @@ from flask_app.models.user import User
 
 @app.route('/')
 def index():
-    users = users.get_all()
+    users = User.get_all()
     print(users)
     return render_template("frontpage/index.html", all_users = users)
 
 @app.route('/user/<string:username>')
 def hello(username):
     return render_template('userPage/index.html', userhtml=username)
+
+@app.route('/signup')
+def signup_form():
+    return render_template('signUp/index.html')
+
+@app.route('/create', methods=['POST'])
+def create_user():
+    if not User.validate_burger(request.form):
+        return redirect('/')
+    User.save(request.form)
--- RezoForge/flask_app/models/user.py
+++ RezoForge/flask_app/models/user.py
@@ -1,15 +1,16 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask import flash
 class User:
-    def __init__( self , data ):
+    def __init__(self , data):
         self.id = data['id']
         self.name = data['name']
         self.username = data['username']
+        self.email = data['email']
         self.location = data['location']
         self.age = data['age']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -18,3 +19,20 @@ class User:
         for user in results:
             users.append( cls(user) )
         return users
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['name']) < 3:
+            flash("Name must be at least 3 characters.")
+            is_valid = False
+        if len(user['username']) < 3:
+            flash("Username must be at least 3 characters.")
+            is_valid = False
+        if len(user['email']) < 3:
+            flash("Email must be at least 3 characters.")
+            is_valid = False
+        if len(user['location']) < 3:
+            flash("Location must be at least 3 characters.")
+            is_valid = False
+        return is_valid
