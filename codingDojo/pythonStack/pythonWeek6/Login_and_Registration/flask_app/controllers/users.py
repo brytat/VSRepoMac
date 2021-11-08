ec7141d91c98638e28db2931ba1a14de85adb214
brytat <brytontateishi@gmail.com>
2021-11-07 23:30:46 -0800

    updating assignment

--- codingDojo/pythonStack/pythonWeek6/Login_and_Registration/flask_app/controllers/users.py
+++ codingDojo/pythonStack/pythonWeek6/Login_and_Registration/flask_app/controllers/users.py
@@ -1,5 +1,5 @@
-from flask_app import app
 from flask import render_template, request, redirect, session
+from flask_app import app
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
@@ -20,7 +20,9 @@ def register_user():
         "email":request.form['email'],
         "password": bcrypt.generate_password_hash(request.form['password']),
     }
-    session['user_id'] = User.create_user(data)
+    id = User.create_user(data)
+    session['user_id'] = id
+
     return redirect('/dashboard')
 
 @app.route('/login', methods=['POST'])
--- codingDojo/pythonStack/pythonWeek6/Login_and_Registration/flask_app/models/user.py
+++ codingDojo/pythonStack/pythonWeek6/Login_and_Registration/flask_app/models/user.py
@@ -22,11 +22,22 @@ class User:
     def create_user(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, passsword) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
         return connectToMySQL(cls.db_name).query_db(query, data)
-    
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for row in results:
+            users.append( cls(row))
+        return users
+        
     @classmethod
     def get_one(cls, data):
-        query = "SELECT * FROM users WHERE id = %(id)s;"
+        query = "SELECT * FROM users WHERE email = %(email)s;"
         user_from_db = connectToMySQL(cls.db_name).query_db(query, data)
+        if len(user_from_db) < 1:
+            return False
         return cls(user_from_db[0])
 
     @staticmethod
