a8593302468b756d1c6ac557fb5446ecd9ca5c0e
brytat <brytontateishi@gmail.com>
2022-02-19 00:23:36 -0800

    /updating repo after MERN stack

---
+++ codingDojo/MERNStack/MERNWeek1/firstJSX/firstjsx/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Get a Black belt in the MERN stack</li>
+        <li>Get a dream job doing software development</li>
+        <li>Work on my personal coding projects</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
