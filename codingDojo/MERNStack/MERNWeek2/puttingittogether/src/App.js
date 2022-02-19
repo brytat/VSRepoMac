333bd1bbc004f3d3591cd3f07d853a39a164cb4c
brytat <brytontateishi@gmail.com>
2022-02-19 15:30:57 -0800

    Purring it together MERN assignment

---
+++ codingDojo/MERNStack/MERNWeek2/puttingittogether/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Person from './components/Person';
+
+const peopleArr = [
+  {
+    firstName: "Joe",
+    lastName: "Smith",
+    age: 28,
+    hairColor: "blonde",
+  },
+  {
+    firstName: "Matilda",
+    lastName: "Jones",
+    age: 33,
+    hairColor: "brown",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Johnson",
+    age: 34,
+    hairColor: "blonde",
+  },
+  {
+    firstName: "Hal",
+    lastName: "Moore",
+    age: 90,
+    hairColor: "grey",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      {peopleArr.map((personObj, index) => (
+        <Person
+          key={index}
+          firstName={personObj.firstName}
+          lastName={personObj.lastName}
+          age={personObj.age}
+          hairColor={personObj.hairColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
