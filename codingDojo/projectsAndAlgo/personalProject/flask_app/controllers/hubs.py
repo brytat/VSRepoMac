73229fb628cab220d600fcbf6a7dd2fc93937ed3
Bryton Tateishi <brytontateishi@gmail.com>
2022-05-02 00:28:18 -0700

    FABTO update

--- codingDojo/projectsAndAlgo/personalProject/flask_app/controllers/hubs.py
+++ codingDojo/projectsAndAlgo/personalProject/flask_app/controllers/hubs.py
@@ -5,7 +5,7 @@ from flask_app.models.hub import Hub
 from flask_app.models.deck import Deck
 
 @app.route('/hub/login', methods=['POST'])
-def process_login():
+def process_hub_login():
     data = {
         'hub_un':request.form['hub_un'],
     }
@@ -16,12 +16,12 @@ def process_login():
     return redirect('/hub/<string:hub_id>')
 
 @app.route('/hub/<string:hub_id>')
-def render_user_page(hub):
+def render_hub_page(hub):
     pageName = "Hub Page"
     if "hub_id" not in session:
         return redirect('/')
     data = {
         "hub_id": session['hub_id']
     }
-    user = Hub.get_one(data)
+    hub = Hub.get_one(data)
     return render_template('hubPage.html', pageName=pageName, hub=hub)
