ee49c6006e0e114b3ffd2bba2ed3cce813bb599e
Bryton Tateishi <brytontateishi@gmail.com>
2022-07-17 00:37:12 -0700

    Front End Update

--- FaBTO/flask_app/controllers/users.py
+++ FaBTO/flask_app/controllers/users.py
@@ -7,13 +7,18 @@ from flask_app.models.deck import Deck
 @app.route('/user/<string:user_id>')
 def render_user_page(user_id):
     pageName = "User Page"
-    if "user_id" not in session:
-        return redirect('/')
+    #This needs tweaking to not have variable arg be determinate of session
+    # if "user_id" not in session:
+    #     return redirect('/')
     data = {
         "user_id": user_id
     }
     user = User.get_one(data)
-    return render_template('userPage.html', pageName=pageName, user=user)
+    data1 = {
+        "user_id": session['user_id']
+    }
+    session_user = User.get_one(data1)
+    return render_template('userPage.html', pageName=pageName, user=user, session=session_user)
 
 @app.route('/user/decks/<string:user_id>')
 def render_decks_page(user_id):
