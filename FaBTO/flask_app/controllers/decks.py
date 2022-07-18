b0ec62507187a9786210704ac347357a23f2cc84
brytat <brytontateishi@gmail.com>
2022-07-17 20:10:32 -0700

    deck CRUD update

--- FaBTO/flask_app/controllers/decks.py
+++ FaBTO/flask_app/controllers/decks.py
@@ -1,4 +1,4 @@
-from flask import url_for, render_template, redirect, request, session
+from flask import url_for, flash, render_template, redirect, request, session
 
 from flask_app import app
 from flask_app.models.deck import Deck
@@ -8,7 +8,8 @@ from flask_app.controllers.users import render_decks_page
 @app.route('/deck/create', methods=['POST'])
 def create_deck():
     if request.form['user_id'] not in session:
-        return redirect('/')
+        flash("You are not able to make decks for another user.")
+        return redirect(url_for('render_decks_page', user_id=request.form['user_id']))
     data = {
         "deck_hero":request.form['deck_hero'],
         "format":request.form['format'],
@@ -18,7 +19,7 @@ def create_deck():
     }
     print(data)
     Deck.create_deck(data)
-    return redirect(url_for(render_decks_page, user_id=session['user_id']))
+    return redirect(url_for('render_decks_page', user_id=request.form['user_id']))
 
 @app.route('/deck/edit/<int:deck_id>')
 def render_edit_deck(deck_id):
